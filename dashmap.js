17d5f68a93b73297de80e7a1b149ddbff4acd64b
El Wero <kolton.rogers.15@gmail.com>
2026-03-04 17:08:38 -0700

    changed vis to false

--- dashmap.js
+++ dashmap.js
@@ -196,7 +196,7 @@ const ipedsHeatmapLayer = new GeoJSONLayer({
 });
 
 const ggrLayer = await createGGRLayer(totalsByStateName);
-ggrLayer.visible = true;
+ggrLayer.visible = false;
 
 const youthLayer = await createYouthLayer(youth18_24ByStateName);
 youthLayer.visible = false;
